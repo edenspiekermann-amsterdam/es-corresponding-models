052c0e53c75357b9bcefe6a8f562c6c9b87d9060
Joseph Preyer <joepreyer@gmail.com>
2018-10-04 15:19:15 +0200

    Fix comment secrets.js

--- src/contentful/secrets.js
+++ src/contentful/secrets.js
@@ -1,6 +1,3 @@
-// These secrets are not kept separate from the repo because these secrets
-// are bundled in the client js. This cannot be changed without changing the
-// current architecture.
 // These tokens are used to read content from the contentful space
 
 module.exports = {
