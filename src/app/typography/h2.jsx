3bf31e4d8640bc14cbfce8be36aa9b3b69a1b269
joepreyer <joepreyer@gmail.com>
2018-10-12 10:13:40 +0200

    Merge basic body view commits

--- src/app/typography/h2.jsx
+++ src/app/typography/h2.jsx
@@ -2,15 +2,9 @@ import { Heading } from './system/heading'
 
 export const H2 = props => (
   <Heading
-<<<<<<< HEAD
     color="emperor"
     fontSize={[4,7]}
     lineHeight={[4,7]}
-=======
-    fontSize={[5]}
-    lineHeight={[6]}
-    mt={[0]}
->>>>>>> dee27f5f61460cc6d9db886a4d384fcd7e1f76c5
     mb={[2]}
     is="h2"
     textStyle="heading"
--- src/models/sections/section-body/section-body-basic-view.jsx
+++ src/models/sections/section-body/section-body-basic-view.jsx
@@ -1,20 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import {Display, H1 } from '../../../app/typography/index'
-import { Box, Flex } from '../../../app/layout/'
-import { Column, Row } from '../../../app/layout/index'
-
-export const SectionBodyBasicView = props => (
-  <Row>
-    <Column>
-      <H1>{props.title}</H1>
-      <Display>Display Test</Display>
-      <Flex justifyContent='center'>
-        <Box width={1/1} maxWidth={1/2}>
-          {props.bodyText}
-        </Box>
-      </Flex>
-=======
 import {H2, Paragraph} from '../../../app/typography/index'
 import {Box} from '../../../app/layout/'
 import {Column, Row} from '../../../app/layout/index'
@@ -26,7 +10,6 @@ export const SectionBodyBasicView = props => (
         <H2>{props.title}</H2>
         <Paragraph>{props.bodyText}</Paragraph>
       </Box>
->>>>>>> dee27f5f61460cc6d9db886a4d384fcd7e1f76c5
     </Column>
   </Row>
 )
